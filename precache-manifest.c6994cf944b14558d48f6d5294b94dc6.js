5fc8b8d98977f9fa9381c4c0108796debc74a5c0
loitd <loitranduc@gmail.com>
2020-10-19 14:34:56 +0700

    Updates

---
+++ precache-manifest.c6994cf944b14558d48f6d5294b94dc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a38a2d68b3247d7d0597b70c76f992",
+    "url": "/reactgithubpage/index.html"
+  },
+  {
+    "revision": "afcc33344a6fd34ae34a",
+    "url": "/reactgithubpage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "517afed4c8c6601f4cf4",
+    "url": "/reactgithubpage/static/js/2.2b9df9aa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/reactgithubpage/static/js/2.2b9df9aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afcc33344a6fd34ae34a",
+    "url": "/reactgithubpage/static/js/main.38a22079.chunk.js"
+  },
+  {
+    "revision": "3d21eec260122ed95d82",
+    "url": "/reactgithubpage/static/js/runtime-main.05181e51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactgithubpage/static/media/logo.5d5d9eef.svg"
+  }
+]);
